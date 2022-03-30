4b93707fa484276c0c2b9cf0b9ba2420b6159b82
Isabellukk <isabellukk@gmail.com>
2022-03-30 04:48:27 -0700

    Completed models, built core functionalities, and built views

--- store/models.py
+++ store/models.py
@@ -1,5 +1,11 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
+
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return super(ProductManager, self).get_queryset().filter(is_active=True)
 
 
 class Category(models.Model):
@@ -8,6 +14,9 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        return reverse('store:category_list', args=[self.slug])
 
     def __str__(self):
         return self.name
@@ -28,10 +37,15 @@ class Product(models.Model):
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+    products = ProductManager()
 
     class Meta:
         verbose_name_plural = 'Products'
         ordering = ('-created',)
+
+    def get_absolute_url(self):
+        return reverse('store:product_detail', args=[self.slug])
 
     def __str__(self):
         return self.title
