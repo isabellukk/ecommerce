cf7f6b25f915ef9839f172cc0bfc36281dcc7567
Isabellukk <isabellukk@gmail.com>
2022-03-30 16:10:46 -0700

    Added resources in footer. Working code thus far. Will begin to build users

--- core/settings.py
+++ core/settings.py
@@ -2,11 +2,9 @@ import os
 from pathlib import Path
 import environ
 
-# Initialise environ variables
 env = environ.Env()
 environ.Env.read_env()
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = env('SECRET_KEY')
