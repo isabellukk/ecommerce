4c038e76445d9d5d0e6346ca8981b8dad8395d62
Isabellukk <isabellukk@gmail.com>
2022-04-01 06:24:17 -0700

    Built account registration app and page

--- account/forms.py
+++ account/forms.py
@@ -2,11 +2,13 @@ from django import forms
 from django.contrib.auth.forms import (AuthenticationForm, PasswordResetForm,
                                        SetPasswordForm)
 
-from .models import
+from .models import UserBase
 
 
 class RegistrationForm(forms.ModelForm):
-
+    first_name = forms.CharField(label='First', max_length=20, required=True)
+    last_name = forms.CharField(
+        label='Last', max_length=20, required=True)
     user_name = forms.CharField(
         label='Enter Username', min_length=4, max_length=50, help_text='Required')
     email = forms.EmailField(max_length=100, help_text='Required', error_messages={
--- account/models.py
+++ account/models.py
@@ -8,7 +8,7 @@ from django_countries.fields import CountryField
 
 class CustomAccountManager(BaseUserManager):
 
-    def create_superuser(self, email, user_name, password, **other_fields):
+    def create_superuser(self, email, user_name, password, first_name, last_name, ** other_fields):
 
         other_fields.setdefault('is_staff', True)
         other_fields.setdefault('is_superuser', True)
@@ -21,26 +21,26 @@ class CustomAccountManager(BaseUserManager):
             raise ValueError(
                 'Superuser must be assigned to is_superuser=True.')
 
-        return self.create_user(email, user_name, password, **other_fields)
+        return self.create_user(email, user_name, password, first_name, last_name, **other_fields)
 
-    def create_user(self, email, user_name, password, **other_fields):
+    def create_user(self, email, user_name, password, first_name, last_name, **other_fields):
 
         if not email:
             raise ValueError(_('You must provide an email address'))
 
         email = self.normalize_email(email)
         user = self.model(email=email, user_name=user_name,
-                          **other_fields)
+                          first_name=first_name, last_name=last_name, **other_fields)
         user.set_password(password)
         user.save()
         return
 
 
 class UserBase(AbstractBaseUser, PermissionsMixin):
-
     email = models.EmailField(_('email address'), unique=True)
     user_name = models.CharField(max_length=150, unique=True)
-    first_name = models.CharField(max_length=150, blank=True)
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
     about = models.TextField(_(
         'about'), max_length=500, blank=True)
     # Delivery details
