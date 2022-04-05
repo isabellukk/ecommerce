aaaae5ef33cc2011016aea371ccaa01b35e922d1
Isabellukk <isabellukk@gmail.com>
2022-04-05 00:35:46 -0700

    Added Account Edit

--- core/settings.py
+++ core/settings.py
@@ -102,3 +102,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 AUTH_USER_MODEL = 'account.UserBase'
 LOGIN_REDIRECT_URL = '/account/dashboard'
 LOGIN_URL = '/account/login/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
